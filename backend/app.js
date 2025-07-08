03913f4e2220b6c5373a99e5145669239c9b62ff
Aryan Neupane <official.aryanneupane@gmail.com>
2025-07-08 20:36:18 +0545

    basic api setup

--- backend/app.js
+++ backend/app.js
@@ -3,16 +3,36 @@ let express =require("express")
 const app = express()
 
 
-
-
-
-app.get('/', (req, res) => {
-  res.send('Hello World')
+app.get('/books', (req, res) => {
+  //logic 
+  res.json({
+    message:"Books fetched",
+    
+  })
 })
 
 
 
+app.post("/books",(req,res)=>{
+  res.json({
+    message:"books uplaoaded Successfully"
+  })
+})
 
+app.delete("/books/:id",(req,res)=>{
+   res.json({
+    message:"Books deleted",
+})
+})
+
+app.patch("/books/:id",(req,res)=>{
+   res.json({
+    message:"Books updated",
+})
+})
+
+
+postgresql="postgresql://postgres.rtelxkdpogfollqvnwxb:#Happiness*100@aws-0-ap-south-1.pooler.supabase.com:6543/postgres"
 
 
 
